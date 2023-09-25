01d77c93e0b86363cd5b91bd2f820af8ff6c67de
Tim Benniks <tbenniks@gmail.com>
2023-09-25 20:35:45 +0200

    block fixes

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sticky top-0 z-30 w-full border-b border-gray-200 bg-white/75">
+  <div class="sticky top-0 z-30 w-full border-b border-gray-200 bg-white/95">
     <div class="mx-auto w-full max-w-screen-xl">
       <nav role="navigation" class="flex justify-center py-3">
         <navigation />
--- storyblok/Block.vue
+++ storyblok/Block.vue
@@ -1,14 +1,53 @@
 <script setup>
 const props = defineProps({ blok: Object });
 const text = computed(() => renderRichText(props.blok.Text));
+const offset = ref("");
+const imageposition = ref("flex-col md:flex-row-reverse");
+const polaroidspace = ref("mt-12 mr-0 self-center");
+const copyspace = ref("ml-0");
+
+if (props.blok.Offset[0] === "left") {
+  offset.value = "relative md:left-20";
+}
+
+if (props.blok.Offset[0] === "right") {
+  offset.value = "relative md:-left-20";
+}
+
+if (props.blok.imageposition[0] === "left") {
+  imageposition.value = "flex-col md:flex-row-reverse";
+  polaroidspace.value = "mt-12 md:mr-12 md:self-start";
+  copyspace.value = "md:ml-0";
+}
+
+if (props.blok.imageposition[0] === "right") {
+  imageposition.value = "flex-col md:flex-row md:justify-between";
+  polaroidspace.value = "mt-12 md:mr-0 md:self-start";
+  copyspace.value = "md:mr-12";
+}
+
+if (props.blok.imageposition[0] === "below") {
+  imageposition.value = "flex-col flex-col items-center";
+  polaroidspace.value = "mt-12 mr-0 self-center";
+  copyspace.value = "ml-0";
+}
 </script>
 
 <template>
-  <section class="max-w-screen-2xl mx-auto text-left" v-editable="blok">
-    <h3 class="text-3xl mb-2">{{ blok.Title }}</h3>
-    <div class="text-base prose xl:text-xl" v-html="text" />
-    <p>{{ blok.Offset[0] }}</p>
-    <p>{{ blok.imageposition[0] }}</p>
-    <pre class="text-left">{{ blok.media }}</pre>
+  <section
+    class="mx-auto text-left mt-44 max-w-11/12"
+    :class="offset"
+    v-editable="blok"
+  >
+    <div class="flex" :class="imageposition">
+      <article :class="copyspace">
+        <h3 class="text-3xl mb-2">{{ blok.Title }}</h3>
+        <div class="text-base prose xl:text-xl text-left" v-html="text" />
+      </article>
+
+      <template v-if="blok.media[0].component === 'Polaroid'">
+        <Polaroid :image="blok.media[0].image" :class="polaroidspace" />
+      </template>
+    </div>
   </section>
 </template>
